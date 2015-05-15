e37c1cce7149f4d61878d7a3e9408249e22bf09f
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-05-15 04:15:26 +0000

    user story 1 progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,45 @@ blocitoff = angular.module('Blocitoff', ["firebase", "ui.router"]);
    });
  }]);
 // add a controller
- blocitoff.controller('home.controller', ['$scope', function($scope) {
+blocitoff.controller('home.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var ref = new Firebase("https://justblocitoff.firebaseio.com/tasks");
 
- }]);
+  $scope.tasks = $firebaseArray(ref);
 
 
+ //var sync = $firebase(ref);
+
+ // create a synchronized (psuedo read-only) array
+// all server changes are downloaded in realtime
+//var tasksArray = sync.$asArray();
+
+// place it in the scope for use in the DOM
+$scope.tasks = tasksArray;
+
+var tasks = sync.$asArray();
+
+
+  $scope.testAdd = function(){
+    tasks.$add({note: 'finish user story 1'});
+    tasks.$add({note: 'grocery shopping'});
+    tasks.$add({note: 'clean bedroom'});
+};
+
+}]);
+
+/*blocitoff.controller('home.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var ref = new Firebase("https://justblocitoff.firebaseio.com/");
+  $scope.tasks = $firebaseArray(ref);
+
+  // var thing = 'whatever';
+ // $scope.task.$add(thing);
+
+  $scope.testAdd = function(){
+    $scope.tasks.$add({note: 'checkpoint'});
+    $scope.tasks.$add({note: 'goodbye'});
+}
+
+}]);*/
 
 
 
